9e3070544d777f27beb8052af1f7ab7ffb2cf6df
thallyssoares <antoniothallys2017@gmail.com>
2023-09-14 19:11:27 -0300

    implementação da verificação dos dados do usuario

---
+++ UserService/src/main/java/infra/VerifyUserAttributes.java
@@ -0,0 +1,33 @@
+package infra;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+public class VerifyUserAttributes implements aplication.VerifyUserAttributes {
+    @Override
+    public boolean verifyAttributes(String name, String email, String cellphone) {
+        boolean isOk = true;
+        if(!((validateEmail(email))&&(name.length()>=4)&&(cellphone.length()>=10&&cellphone.length()<=11))){
+            isOk = false;
+        }
+        return isOk;
+    }
+    public boolean validateEmail(String email){
+
+        boolean ok = true;
+        try{
+            InternetAddress emailValidator = new InternetAddress(email);
+            emailValidator.validate();
+        } catch (AddressException ex){
+            ok = false;
+        }
+        return ok;
+
+    }
+    @Override
+    public boolean verifyLoginAttributes(String email, String password) {
+        boolean isLoginOk = true;
+        if(!(validateEmail(email)&&(!password.isEmpty()))) isLoginOk = false;
+        return isLoginOk;
+    }
+}
