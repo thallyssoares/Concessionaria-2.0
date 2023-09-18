ab9bd7f4caa98f52d368aee749511295bb412e36
thallyssoares <antoniothallys2017@gmail.com>
2023-09-18 19:03:52 -0300

    criado o metodo para fechar as conexoes do db

--- UserService/src/main/java/core/db/ConnectionJDBC.java
+++ UserService/src/main/java/core/db/ConnectionJDBC.java
@@ -3,6 +3,7 @@ package core.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public final class ConnectionJDBC {
     private static String url;
@@ -22,12 +23,20 @@ public final class ConnectionJDBC {
         }
         return null;
     }
-    public static void closeConnection(Connection connection) {
+    public static void close(Connection connection) {
         try {
             connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-
+    public static void close(Connection connection, Statement stmt){
+        close(connection);
+        try {
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+}
