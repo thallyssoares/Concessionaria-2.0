a9df6ef48fffbc5bc82614f4c0ec5a23ad5098c3
thallyssoares <antoniothallys2017@gmail.com>
2023-09-18 19:05:27 -0300

    reorganizado o exception do metodo

--- UserService/src/main/java/infra/VerifyUserAttributes.java
+++ UserService/src/main/java/infra/VerifyUserAttributes.java
@@ -7,11 +7,10 @@ import javax.mail.internet.InternetAddress;
 
 public class VerifyUserAttributes implements aplication.VerifyUserAttributes {
     @Override
-    public boolean verifyAttributes(String name, String email, String cellphone)  throws InvalidUserAttributes {
+    public boolean verifyAttributes(String name, String email, String cellphone) throws InvalidUserAttributes{
         boolean isOk = true;
         if(!((validateEmail(email))&&(name.length()>=4)&&(cellphone.length()>=10&&cellphone.length()<=11))){
             isOk = false;
-            throw new InvalidUserAttributes();
         }
         return isOk;
     }
